6e00b75b4ba58067b93548f6e8a4cc360f9662b6
Adrien <chemladrien@gmail.com>
2018-06-05 16:42:51 +0200

    Auto stash before merge of "master" and "origin/master"

--- form.php
+++ form.php
@@ -1,19 +1,30 @@
 <?php
     require_once("header.php");
-    
+    $user = 8;
+    $id = $db->prepare("SELECT COUNT(id) as count FROM compte_bancaire WHERE id_user = ?");
+    $id->execute(array($user));
+    $count = $id->fetch()['count'];
+
     $name   = $_POST["nom"];
     $type   = $_POST["type"];
     $solde  = $_POST["solde"];
     $choix  = $_POST["devise"];
 
-    $req= $db->prepare("INSERT INTO compte_bancaire (nom, type, solde, devise) VALUES (:nom, :type, :solde, :devise)");
-    $req->execute(array(
-        "nom" => $name,
-        "type" => $type,
-        "solde" => $solde,
-        "devise" => $choix
-    ));
-
-    if($req->rowCount() > 0) {echo ("Vos données ont bien été enregistrées !");}
-    else {echo "Erreur à l'ajout";}
+    if($count < 10){
+        $req = $db->prepare("INSERT INTO compte_bancaire (nom, type, solde, devise, id_user) VALUES (:nom, :type, :solde, :devise, :id_user)");
+        $req->execute(array(
+            "nom" => $name,
+            "type" => $type,
+            "solde" => $solde,
+            "devise" => $choix,
+            "id_user" => $user
+        ));
+    if($req->rowCount() > 0) {
+        echo ("Vos données ont bien été enregistrées !");
+    } else {
+        echo "Erreur à l'ajout";
+    }
+} else {
+    echo "Vous avez atteint la limite maximum de comptes bancaires !";
+}
 ?>
